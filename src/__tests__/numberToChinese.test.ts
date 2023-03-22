ce805cb91adcb64270eeddaa8e4ca575b95e0e93
youngluo <youngluooo@gmail.com>
2023-03-22 21:27:46 +0800

    feat: add numberToChinese

--- src/__tests__/numberToChinese.test.ts
+++ src/__tests__/numberToChinese.test.ts
@@ -1,27 +1,43 @@
 import { numberToChinese } from '../numberToChinese'
 
 describe('numberToChinese', () => {
-  // test('numberToChinese(100010001) === 一亿零一万零一', () => {
-  //   expect(numberToChinese(100010001)).toBe('一亿零一万零一')
-  // })
+  test('numberToChinese(1000000000000) === 无效的数字或数字过大', () => {
+    expect(numberToChinese(1000000000000)).toBe('无效的数字或数字过大')
+  })
+
+  test('numberToChinese(10010000001) === 一百亿一千万零一', () => {
+    expect(numberToChinese(10010000001)).toBe('一百亿一千万零一')
+  })
+
+  test('numberToChinese(100010001) === 一亿零一万零一', () => {
+    expect(numberToChinese(100010001)).toBe('一亿零一万零一')
+  })
 
   test('numberToChinese(100001) === 十万零一', () => {
     expect(numberToChinese(100001)).toBe('十万零一')
   })
 
-  // test('numberToChinese(10000) === 一万', () => {
-  //   expect(numberToChinese(10000)).toBe('一万')
-  // })
+  test('numberToChinese(100000000) === 一亿', () => {
+    expect(numberToChinese(100000000)).toBe('一亿')
+  })
 
-  // test('numberToChinese(100) === 一百', () => {
-  //   expect(numberToChinese(10)).toBe('一百')
-  // })
+  test('numberToChinese(111111111) === 一亿一千一百一十一万一千一百一十一', () => {
+    expect(numberToChinese(111111111)).toBe('一亿一千一百一十一万一千一百一十一')
+  })
 
-  // test('numberToChinese(10) === 十', () => {
-  //   expect(numberToChinese(10)).toBe('十')
-  // })
+  test('numberToChinese(10000) === 一万', () => {
+    expect(numberToChinese(10000)).toBe('一万')
+  })
 
-  // test('numberToChinese(1) === 一', () => {
-  //   expect(numberToChinese(1)).toBe('一')
-  // })
+  test('numberToChinese(100) === 一百', () => {
+    expect(numberToChinese(100)).toBe('一百')
+  })
+
+  test('numberToChinese(10) === 十', () => {
+    expect(numberToChinese(10)).toBe('十')
+  })
+
+  test('numberToChinese(1) === 一', () => {
+    expect(numberToChinese(1)).toBe('一')
+  })
 })
--- src/numberToChinese.ts
+++ src/numberToChinese.ts
@@ -1,8 +1,7 @@
-/**
- * 实现一个函数，将数字转为中文输出，不超过 10000 亿
- * numberToChinese(100010001) ==> 一亿零一万零一
- */
-export function numberToChinese(num: number): string {
+export function numberToChinese(num: number) {
+  if (isNaN(num) || typeof num !== 'number' || num >= 1000000000000) {
+    return '无效的数字或数字过大'
+  }
   const chnNumber = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九']
   const chnUnits = ['', '十', '百', '千']
   const chnGroupUnits = ['', '万', '亿']
@@ -17,22 +16,32 @@ export function numberToChinese(num: number): string {
 
   while (num > 0) {
     const digit = num % 10
+
     if (digit !== 0) {
       result = chnNumber[digit] + chnUnits[unitIndex] + result
-    } else {
-      if (result[0] !== chnNumber[0]) {
-        result = chnNumber[0] + result
-      }
-    }
-
-    if (unitIndex++ === 4) {
-      groupIndex++
-      result = chnGroupUnits[groupIndex] + result
-      unitIndex = 0
+      // 排除“零零”、“零万”、“零亿”
+    } else if (
+      result.length > 0 &&
+      result[0] !== chnNumber[0] &&
+      result[0] !== chnGroupUnits[groupIndex]
+    ) {
+      result = chnNumber[0] + result
     }
 
     num = Math.floor(num / 10)
+
+    if (num > 0 && ++unitIndex === 4) {
+      // 排除“亿万”
+      if (groupIndex > 0 && chnGroupUnits[groupIndex] === result[0]) {
+        result = result.slice(1)
+      }
+      // 加零处理，例如 1001000、10010000000
+      // const zero =
+      //   num % 100 === 0 && result.length > 0 && result[0] !== chnNumber[0] ? chnNumber[0] : ''
+      result = chnGroupUnits[++groupIndex] + result
+      unitIndex = 0
+    }
   }
 
-  return result
+  return result.replace(/^一十/, '十')
 }
